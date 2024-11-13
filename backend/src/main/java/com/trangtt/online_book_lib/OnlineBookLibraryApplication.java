1063a176145cf18d23128144b9abcafa04448ce6
trinhtrang2710 <trinhtrang371@gmail.com>
2024-11-13 18:14:29 +0700

    implement codebase

---
+++ backend/src/main/java/com/trangtt/online_book_lib/OnlineBookLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.trangtt.online_book_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookLibraryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/trangtt/online_book_lib/OnlineBookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trangtt.online_book_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
