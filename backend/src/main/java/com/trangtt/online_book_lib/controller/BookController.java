875b51efe98071baa0dc69d090996be251403bc8
trinhtrang2710 <trinhtrang371@gmail.com>
2024-11-17 21:41:40 +0700

    book manage

---
+++ backend/src/main/java/com/trangtt/online_book_lib/controller/BookController.java
@@ -0,0 +1,39 @@
+package com.trangtt.online_book_lib.controller;
+
+import com.trangtt.online_book_lib.model.Book;
+import com.trangtt.online_book_lib.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        return bookService.saveBook(book);
+    }
+
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Long id) {
+        bookService.deleteBook(id);
+    }
+}
---
+++ backend/src/main/java/com/trangtt/online_book_lib/service/BookService.java
@@ -0,0 +1,34 @@
+package com.trangtt.online_book_lib.service;
+
+import com.trangtt.online_book_lib.model.Book;
+import com.trangtt.online_book_lib.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id).orElse(null);
+    }
+
+    public void deleteBook(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
